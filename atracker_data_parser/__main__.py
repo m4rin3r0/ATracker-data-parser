81fa804c98263271db2b4f9aed41e1100565805f
Leon Mac <leon.seemann@icloud.com>
2026-01-17 18:46:33 +0100

    added more options for cli

--- atracker_data_parser/__main__.py
+++ atracker_data_parser/__main__.py
@@ -2,10 +2,18 @@ import click
 from atracker_data_parser.time_data_parser import TimeDataParser
 
 @click.command()
-@click.option("--filepath", "-f", required=True, help="path to sqlite-file of time-data")
-def main(filepath):
-    data = TimeDataParser().parse(filepath)
-    print(data.head())
+@click.option(
+    "--backup",
+    "--filepath",
+    "-b",
+    "-f",
+    "backup_path",
+    required=True,
+    help="path to ATracker backup (.ATracker) archive",
+)
+def main(backup_path):
+    data = TimeDataParser().parse(backup_path)
+    pass
 
 if __name__ == "__main__":
     main()
